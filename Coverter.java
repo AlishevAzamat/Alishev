2e7f38c5eace3cb364530cfd7ae2168304c604b2
Алишев Азамат <aalishevaa@yandex.ru>
2022-09-30 13:59:45 +0600

    Erorr

---
+++ Coverter.java
@@ -0,0 +1,4 @@
+public class Coverter {
+    int aza = 3;
+    String awe = "wed";
+}
--- Praktikum.java
+++ Praktikum.java
@@ -1,5 +1,7 @@
-public class Main {
+public class Praktikum {
 
     public static void main(String[] args) {
+        DinnerAdvisor alishev = new DinnerAdvisor();
+        Coverter cover = new Coverter();
     }
 }
